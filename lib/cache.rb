7c33e242785238ac7b2a743a617b485bbec34b79
FromAtom <fromatom@gmail.com>
2022-08-15 18:57:52 +0900

    RedisToGoの代わりにHeroku Redisを利用する (#14)
    * app.jsonのaddonでHeroku Redisを導入するように変更
    
    * RedisのURLを取得する環境変数をREDIS_URLに変更

--- lib/cache.rb
+++ lib/cache.rb
@@ -2,7 +2,7 @@ require 'time'
 require 'redis'
 
 module Cache
-  REDIS_URL = ENV['REDISTOGO_URL'] || "redis://127.0.0.1:6379/0"
+  REDIS_URL = ENV['REDIS_URL'] || "redis://127.0.0.1:6379/0"
   KEY_PREFIX = 'utsushie-'
 
   def self.set(key)
